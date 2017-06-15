adc6e99d5f7d8dd1ef82f53f762bcf535b074a5e
student <student@K0312.step.lan>
2017-06-15 21:09:12 +0300

    case

--- TimeTable/src/org/itstep/WorkWithGroup.java
+++ TimeTable/src/org/itstep/WorkWithGroup.java
@@ -3,29 +3,58 @@ package org.itstep;
 public class WorkWithGroup {
 
 	public static void main(String[] args) {
-		
+
 		GroupStudent group = new GroupStudent();
 		group.setName("ST20");
-		
-		for (int i=0; i<15;  i++){
-			Student student = new Student(("Baby"+(i+1)), ("myPass"+(i+1)), "Alex", "Kiev");
-			if(i>8){
-				student = new Student(("Baby"+(i+1)), ("myPass"+(i+1)), "Alex", "Ternopil");
+
+		for (int i = 0; i < 15; i++) {
+			Student student = new Student(("Baby" + (i + 1)), ("myPass" + (i + 1)), "Alex", "Kiev");
+			if (i > 8) {
+				student = new Student(("Baby" + (i + 1)), ("myPass" + (i + 1)), "Alex", "Ternopil");
 			}
-		group.addStudentToGroup(student);	
-			
+			group.addStudentToGroup(student);
+
 		}
-System.out.println("My best group is "+ group.getName());
-System.out.println("List of Students: ");
+		System.out.println("My best group is " + group.getName());
+		System.out.println("List of Students: ");
 
-for(Student student : group.getStudents()){
-	if(student.getCity().equals("Kiev")){
-	
-	System.out.println("Login: " + student.getLogin() + " Name: " + student.getName()+ " .I'm from " +student.getCity());
-	System.out.println(student.hashCode());
-}
-		
+		if (!group.getStudents().isEmpty())
+
+			for (Student student : group.getStudents()) {
+				switch(student.getCity()){
+				case "Kiev":
+				System.out.println("Login: " + student.getLogin() + " Name: " + student.getName() + " .I'm from "
+						+ student.getCity());
+				break;
+				default:
+					System.out.println("I don't know where I from...");
+					break;
+					
+				}
+				
+				
+				
+				
+				if (!student.getCity().equals("Kiev")) {
+
+					// System.out.println("Login: " + student.getLogin() + "
+					// Name: " + student.getName()+ " .I'm from "
+					// +student.getCity());
+					// System.out.println(student.hashCode());
+					// }
+					// else{
+					System.out.println("Login: " + student.getLogin() + " Name: " + student.getName() + " .I'm from "
+							+ student.getCity());
+
+				}
+				if (!student.getCity().contains("Tern")) {
+					System.out.println("Login: " + student.getLogin() + " Name: " + student.getName() + " .I'm from "
+							+ student.getCity());
+					
+				} else {
+
+				}
+			}
+
 	}
-
-}
 }
